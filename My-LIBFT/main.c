7736b6455739a907b627dcc668358ac95962dbba
vde-frei <vde-frei@student.42sp.org.br>
2023-11-27 07:50:05 -0300

    fixed makefile

--- My-LIBFT/main.c
+++ My-LIBFT/main.c
@@ -5,16 +5,5 @@
 
 int main (void)
 {
-	int	fd;
-	char	*line;
-
-	fd = open("test.txt", O_RDONLY);
-	while ((line = get_next_line(fd)))
-	{
-		printf("%s\n", line);
-		free(line);
-	}
-	close(fd);
-	ft_putstr_fd("Press any key to exit..\n", 1);
-	return 0;
+	printf("hello agora vai porra!");
 }
